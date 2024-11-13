ac55cf08cb13d090f00e568e6ded947cbb8bc5e9
tutamdev <tutamdev@gmail.com>
2024-11-13 18:54:58 +0700

    update nav

--- src/admin_components/Nav/Nav.jsx
+++ src/admin_components/Nav/Nav.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { LogoutOutlined, QuestionCircleOutlined, SettingOutlined } from '@ant-design/icons';
 import { Avatar, Button, Dropdown, Popconfirm, Space } from 'antd';
+import LocalStorageService from '../../services/LocalStorageService';
 
 export const Nav = () => {
     const handleLogout = () => {
         console.log("logout");
         // TODO
     }
-
+    const userLogged = LocalStorageService.getItem("userLogged");
     return (
         <div className='flex h-14 items-center space-x-4 p-2 justify-end border-b border-gray-200 bg-gray-50'>
             <div className='flex-grow text-center text-xl font-bold'>Trang Quản Trị</div>
             <div>
-                <h4 className='font-bold'>ADMIN</h4>
-                <p className='text-xs font-light'>ROLE: ADMINISTRATOR</p>
+                <h4 className='font-bold'>{userLogged.fullName}</h4>
+                <p className='text-xs font-light text-right'>ROLE: {userLogged.role}</p>
             </div>
             <Avatar
                 src="https://img.tripi.vn/cdn-cgi/image/width=700,height=700/https://gcs.tripi.vn/public-tripi/tripi-feed/img/474082nAQ/avatar-duong-tang-bua_044338241.jpg"
